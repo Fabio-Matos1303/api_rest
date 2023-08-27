8f465d5f8660742b38b685917f76024df78dfc69
Fabio-Matos1303 <fabiosmo123@gmail.com>
2023-08-27 10:20:38 -0300

    Commit inicial

---
+++ server.js
@@ -0,0 +1,8 @@
+import app from './app';
+
+const port = '3001';
+app.listen(port, () => {
+  console.log();
+  console.log(`Server is running on port ${port}`);
+  console.log(`http://localhost:${port}`);
+});
