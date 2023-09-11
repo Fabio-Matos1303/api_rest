86c32142c4bbd727bba00c29e159373765321bbe
Fabio-Matos1303 <fabiosmo123@gmail.com>
2023-09-11 07:44:05 -0300

    CRUD de alunos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -2,9 +2,98 @@ import Aluno from "../models/Aluno";
 
 class AlunoController {
   async index(req, res) {
-    const alunos = Aluno.findAll();
+    const alunos = await Aluno.findAll();
     res.send(alunos);
   }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID inválido']
+        })
+      }
+
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno inválido']
+        })
+      }
+
+      return res.json(aluno)
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message)
+      })
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID inválido']
+        })
+      }
+
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno inválido']
+        })
+      }
+
+      await aluno.destroy();
+
+      return res.json({
+        apagado: true
+      })
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message)
+      })
+    }
+  }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.json(aluno)
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message)
+      })
+    }
+   }
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID inválido']
+        })
+      }
+
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno inválido']
+        })
+      }
+
+      const alunoAtualizado = await aluno.update(req.body);
+
+      return res.json(alunoAtualizado)
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message)
+      })
+    }
+   }
 }
 
 export default new AlunoController();
--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -6,5 +6,9 @@ import loginRequired from "../middlewares/loginRequired"
 const router = new Router();
 
 router.get('/', alunoController.index);
+router.put('/:id', alunoController.update);
+router.get('/:id', alunoController.show);
+router.delete('/:id', alunoController.delete);
+router.post('/', alunoController.store);
 
 export default router;
